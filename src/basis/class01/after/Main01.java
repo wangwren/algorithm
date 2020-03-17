c5206b06f04d2bbfcde8a38b3d8300dd928b5bcf
wangwren <291762380@qq.com>
2020-03-17 20:24:26 +0800

    添加题目

--- src/basis/class01/after/Main01.java
+++ src/basis/class01/after/Main01.java
@@ -4,6 +4,57 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * 查找和排序
+ *
+ * 题目：输入任意（用户，成绩）序列，可以获得成绩从高到低或从低到高的排列,相同成绩
+ * 都按先录入排列在前的规则处理。
+ *
+ * 示例：
+ * jack      70
+ * peter     96
+ * Tom       70
+ * smith     67
+ *
+ * 从高到低  成绩
+ * peter     96
+ * jack      70
+ * Tom       70
+ * smith     67
+ *
+ * 从低到高
+ *
+ * smith     67
+ *
+ * jack      70
+ * Tom      70
+ * peter     96
+ *
+ *
+ * 输入描述:
+ * 输入多行，先输入要排序的人的个数，然后输入排序方法0（降序）或者1（升序）再分别输入他们的名字和成绩，以一个空格隔开
+ *
+ *
+ *
+ * 输出描述:
+ * 按照指定方式输出名字和成绩，名字和成绩之间以一个空格隔开
+ *
+ * 示例1
+ *
+ * 输入
+ *
+ * 3
+ * 0
+ * fang 90
+ * yang 50
+ * ning 70
+ *
+ * 输出
+ *
+ * fang 90
+ * ning 70
+ * yang 50
+ */
 public class Main01 {
 
     public class User{
