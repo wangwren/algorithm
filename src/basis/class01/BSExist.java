cc2484e2c9477e032b4bda33ade310ff2bed443f
wangwren <wangwren0906@gmail.com>
2020-03-20 18:16:36 +0800

    二分法

---
+++ src/basis/class01/BSExist.java
@@ -0,0 +1,25 @@
+package basis.class01;
+
+/**
+ * 二分法：
+ *      从有序数组中搜索元素
+ */
+public class BSExist {
+    public static boolean exist(int[] sortArr,int num){
+        int L = 0;
+        int R = sortArr.length - 1;
+
+        while (L < R){
+            int mid = L + ((R - L) >> 1);
+            if (sortArr[mid] == num){
+                return true;
+            } else if (sortArr[mid] > num){
+                R = mid - 1;
+            } else {
+                L = mid + 1;
+            }
+        }
+
+        return sortArr[L] == num;
+    }
+}
---
+++ src/basis/class01/BSNearLeft.java
@@ -0,0 +1,24 @@
+package basis.class01;
+
+/**
+ * 使用二分，查找>=某个数最左侧的位置
+ */
+public class BSNearLeft {
+    public static int nearLeft(int[] sortArray,int num){
+        int L = 0;
+        int R = sortArray.length - 1;
+        int index = -1; //记录最左的位置
+        while (L <= R){
+            int mid = (L + (R - L) >> 2);
+            if (sortArray[mid] >= num){
+                //如果大于等于num，就记录一下这个位置，之后继续循环找
+                index = mid;
+                R = mid - 1;
+            } else {
+                L = mid + 1;
+            }
+        }
+
+        return index;
+    }
+}
